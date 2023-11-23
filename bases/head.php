a2f8ae3fe3a126ba83ab26f1907d3271c5bc8091
Flavia Jamily dos Santos Macena <flavia.jamily.2005@gmail.com>
2023-11-22 21:53:54 -0300

    Modularização do sistema

---
+++ bases/head.php
@@ -0,0 +1,76 @@
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!--Importação da fonte-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Coustard&family=Inter:wght@200;400;600&family=Lexend:wght@300;400;600&display=swap" rel="stylesheet">
+    <!--Ícone da barra do navegador-->
+    <link rel="icon" type="image/x-icon" href="src/imagens/flexicon.png">
+    <!--Ícones do footer-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!--CSS-->
+    <link rel="stylesheet" type="text/css" href="src/css/potiempresta.css" />
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+
+      .b-example-divider {
+        width: 100%;
+        height: 3rem;
+        background-color: rgba(0, 0, 0, .1);
+        border: solid rgba(0, 0, 0, .15);
+        border-width: 1px 0;
+        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+      }
+
+      .b-example-vr {
+        flex-shrink: 0;
+        width: 1.5rem;
+        height: 100vh;
+      }
+
+      .bi {
+        vertical-align: -.125em;
+        fill: currentColor;
+      }
+
+      .nav-scroller {
+        position: relative;
+        z-index: 2;
+        height: 2.75rem;
+        overflow-y: hidden;
+      }
+
+      .nav-scroller .nav {
+        display: flex;
+        flex-wrap: nowrap;
+        padding-bottom: 1rem;
+        margin-top: -1px;
+        overflow-x: auto;
+        text-align: center;
+        white-space: nowrap;
+        -webkit-overflow-scrolling: touch;
+      }
+
+      .bd-mode-toggle {
+        z-index: 1500;
+      }
+
+      .bd-mode-toggle .dropdown-menu .active .bi {
+        display: block !important;
+      }
+    </style>
---
+++ bases/rodape.php
@@ -0,0 +1,59 @@
+      <!--Rodapé-->
+      <footer>
+            <div id="footer_content">
+                <div id="footer_contacts">
+                    <h1><img src="src/imagens/logo.png"  width="190" height="auto" alt="logo do quiz"></h1>
+                    <p class="redes">Conheça melhor o Potimaker:</p>
+ 
+                    <div id= "footer_social_media">
+                        <a href="https://www.instagram.com/potimaker.ifrn/" class="footer-link" id="instagram">
+                            <i class="fa-brands fa-instagram"></i>
+                        </a>
+ 
+                        <a href="https://www.youtube.com/@potimakerifrn" class="footer-link" id="youtube">
+                            <i class="fa-brands fa-youtube"></i>
+                        </a>
+ 
+                        <a href="https://potimaker-ifrn.github.io/sitePotimaker/index.html" class="footer-link" id="site">
+                            <img src="src/imagens/LogoPM(branca).png" alt="Site do Potimaker" width=40px>
+                        </a>
+                    </div>
+                </div>
+ 
+                <ul class="footer-list">
+                    <li>
+                        <h3>Desenvolvido por</h3>
+                    </li>
+                    <li>
+                        <a href="mailto:brito.beatriz@escolar.ifrn.edu.br" class="footer-link" target="blank">Emilly Beatriz</a>
+                    </li>
+                    <li>
+                        <a href="mailto:flavia.macena@escolar.ifrn.edu.br" class="footer-link" target="blank">Flavia Jamily</a>
+                    </li>
+                    <li>
+                        <a href="mailto:isabel.natalia@escolar.ifrn.edu.br" class="footer-link" target="blank">Isabel Natalia</a>
+                    </li>
+                    <li>
+                        <a href="mailto:matias.rosendo@escolar.ifrn.edu.br" class="footer-link" target="blank">Letícia Matias</a>
+                    </li>
+                    <li>
+                        <a href="mailto:lima.sarah@escolar.ifrn.edu.br" class="footer-link" target="blank">Sarah Lima</a>
+                    </li>
+                </ul>
+ 
+                <ul class="footer-list">
+                    <li>
+                        <h3>Ficha técnica</h3>
+                    </li>
+                    <li>
+                        <p>Trabalho realizado para a disciplina de programação para internet</p>
+                    </li>
+                    <li>
+                        <p>Professor: Bruno Gomes</p>
+                    </li>
+                    <li>
+                        <p>IFRN- Campus Canguaretama</p>
+                    </li>
+                </ul>
+            </div>
+        </footer>

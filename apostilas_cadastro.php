07672b9b2355488049b3d4987a46fbd6375d46d6
Sarah  Lima <lima.sarah@escolar.ifrn.edu.br>
2023-11-28 16:20:10 -0300

    Página de cadastro das apostilas feita

--- apostilas_cadastro.php
+++ apostilas_cadastro.php
@@ -1,30 +1,175 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<?php include 'bases/head.php' ?>
+<title>Cadastro</title>
+<style>
+      /*Cadastro*/
+      body{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    background-image: url(src/imagens/fundo_apo.png);
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    align-items: center;
+    justify-content: center;
+}
 
-<div class="box"> 
+.box{
+    display: flex;
+    margin-top: 50px;
+    padding: 40px;
+}
+
+.img-box{
+    background-color: rgba(255, 255, 255, 0.5);
+    width: 50%;
+    height: 135%;
+    display: flex;
+    align-items: center;
+    padding: 20px;
+    border-radius: 20px  0 0 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.form-box{
+    background-color: rgba(255, 255, 255, 0.2);
+    backdrop-filter: blur(40px);
+    padding: 30px 40px;
+    width: 50%;
+    height: 135%;
+    border-radius: 0 20px 20px 0;
+}
+
+.form-box h2{
+    font-size: 30px;
+    color: #35B2AE;
+    font-family: 'Lexend', sans-serif;
+}
+
+.form-box form{
+    margin: 20px 0;
+}
+
+form .input-group{
+    margin-bottom: 15px;
+}
+
+form .input-group label{
+    color: #369D9A;
+    font-weight: bold;
+    display: block;
+    margin-bottom: 5px;
+}
+
+form .input-group input{
+    width: 100%;
+    height: 47px;
+    background-color: rgba(255, 255, 255, 0.32);
+    border-radius: 20px;
+    outline: none;
+    border: 2px solid transparent;
+    padding: 15px;
+    font-size: 15px;
+    color: #616161;
+    transition: all 0.4s ease;
+    font-family: 'Inter', sans-serif;
+}
+
+form .input-group input:focus{
+    border-color: #5EA0C5;
+}
+
+.criar input[type="submit"]{
+    width: 100%;
+    height: 47px;
+    background: #62C1CD;
+    border-radius: 20px;
+    outline: none;
+    border: none;
+    margin-top: 15px;
+    color: white;
+    cursor: pointer;
+    font-size: 16px;
+    font-family: 'Lexend', sans-serif;
+    text-transform: uppercase;
+    font-weight: bold;
+    box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.2);
+}
+
+input[type="submit"]:hover {
+    background-color: #ffff; 
+    color: #62C1CD;
+}
+
+.login{
+  color: #3598B2;
+  text-decoration: none;
+  font-weight: bold;
+}
+
+.login:hover{
+  color: #0EBDB8;
+}
+
+@media (max-width:930px){
+    .img-box{
+        display: none;
+    }
+    .box{
+        width: 610px;
+    }
+    .form-box{
+        width: 100%;
+        border-radius: 20px;
+    }
+}
+</style>
+</head>
+
+<body>
+
+<?php include 'bases/menu.php' ?>
+
+  <!--Cadastro de apostilas-->
+  <div class="box">
+        <div class="img-box">
+            <img src="src/imagens/icon_apo.png" width="550px" height="auto" alt="Ícone de cadastro">
+        </div>
         <div class="form-box">
-        <h2><i class="fa-solid fa-user fa-beat icon"></i>  Cadastre uma Apostila</h2>
-
+        <h2> <i class="fa-solid fa-book fa-beat"></i> Cadastro de Apostilas</h2>
         <!--formulário--> 
-
-            <form action="apostilas_admin.php" method="post"> <!--falta matricula pq imagino que tenha que ter varias coisas--> 
+            <form action="usuario_cadastro.php" method="post">
                 <div class="input-group">
                     <label for="nome">Nome:</label>
-                    <input type="text" id="nome" name="nome" placeholder="Digite nome da apostila" required>
+                    <input type="text" id="nome" name="nome" placeholder="Digite o nome da apostila" required>
                 </div>
                 <div class="input-group">
-                    <label for="Quantidade">Quantidade</label>
-                    <input type="number" id="Quantidade" name="Quantidade" placeholder="Quantos exemplares há no estoque?" required>
+                    <label for="email">Quantidade:</label>
+                    <input type="number" id="quantidade" name="quantidade" placeholder="Quantidade de apostilas disponíveis para empréstimo" required>
                 </div>
                 <div class="input-group">
-                    <label for="matrícula">ISBN:</label>
-                    <input type="number" id="isbn" name="isbn" placeholder="Digite a isbn da apostila" required>
+                    <label for="descrição">Descrição:</label>
+                    <input type="text" id="descricao" name="descricao" placeholder="Digite uma pequena descrição da apostila" required>
                 </div>
                 <div class="input-group">
-                    <label for="Descricao">Descricao:</label>
-                    <input type="text" id="Descricao" name="Descricao" placeholder="Esreva Descricao" required>
+                    <label for="isbn">ISBN:</label>
+                    <input type="number" id="isbn" name="isbn" placeholder="Digite o ISBN da apostila (13 dígitos)" required>
                 </div>
                 <div class="criar">
-                    <input type="submit" name="criar" value="Cadastrar">
+                    <input type="submit" name="criar" value="Criar conta">
                 </div>
             </form>
         </div>
     </div>
+
+  <div class="espaço"></div>
+  
+  <?php include 'bases/rodape.php' ?>
+
+</body>
+</html>
